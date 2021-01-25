2a427ff27198f4c152adf111ce81ad03f0202a41
odailsonsilva <odafreelancerti@gmail.com>
2021-01-25 15:20:35 -0300

    feat: configuracao das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './modules/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SignInComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
