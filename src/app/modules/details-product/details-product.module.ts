87a9fe79a854bb31fae021a43667b17e89983bb4
odailsonsilva <odafreelancerti@gmail.com>
2021-01-28 14:39:09 -0300

    feat: logica remocao e adicao

--- src/app/modules/details-product/details-product.module.ts
+++ src/app/modules/details-product/details-product.module.ts
@@ -1,9 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { AddProductComponent } from "src/app/shared/components/details/add-product/add-product.component";
 import { DetailsProductComponent } from "./details-product.component";
 
 @NgModule({
-  declarations: [ DetailsProductComponent ],
+  declarations: [
+    DetailsProductComponent,
+    AddProductComponent
+   ],
   imports: [ CommonModule ],
   exports: [ DetailsProductComponent ]
 })
--- src/app/shared/components/details/add-product/add-product.component.ts
+++ src/app/shared/components/details/add-product/add-product.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProductComponent implements OnInit {
 
+  quantityItem: number = 1;
+  price: number = 3.25;
+  total: string = 3.25.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  removeItem() {
+    if(this.quantityItem !== 1){
+      this.quantityItem -= 1
+      let som = this.price * this.quantityItem
+      this.total = som.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+    }
+  }
+
+  addItem() {
+    this.quantityItem += 1
+
+    let som = this.price * this.quantityItem
+    this.total = som.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+  }
 }
